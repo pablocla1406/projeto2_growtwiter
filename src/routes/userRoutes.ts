bb43b59bf759240d897cb39a23112eb19cc83e41
pablocla1406 <pabloarruda374@gmail.com>
2025-07-30 11:59:38 -0300

    Adicionado as rotas de auth e user. Pronto para o teste

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/userController';
+import { authenticateToken } from '../middlewares/auth';
+
+const router = Router();
+
+router.use(authenticateToken);
+
+router.get('/:id', UserController.getUser);
+
+router.post('/:id/follow', UserController.followUser);
+
+router.delete('/:id/follow', UserController.unfollowUser);
+
+export default router;
